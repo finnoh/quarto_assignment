7ee53e1b89a657fb228afa5117d8544a2bdcc134
Finn-Ole Höner <finn.ole.hoener@gmail.com>
2023-02-06 12:56:36 +0100

    Update main.r

--- main.r
+++ main.r
@@ -1,9 +1,3 @@
 # Setup -----------------------------------------
-# load and install packages
-if (!require(pacman)) {
-    install.packages("pacman")
-}
-pacman::p_load(tidyverse)
-
 # source the toolbox
 source("./dev/toolbox.r")
