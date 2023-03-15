5d204041fbbdd40ea87fde7ddc29757d9b928109
Finn-Ole Höner <finn.ole.hoener@gmail.com>
2023-03-15 10:25:14 +0100

    Update toolbox.r
    table function and mytheme

--- dev/toolbox.r
+++ dev/toolbox.r
@@ -2,6 +2,15 @@
 if (!require(pacman)) {
     install.packages("pacman")
 }
-pacman::p_load(tidyverse, ggpubr)
+pacman::p_load(tidyverse, ggpubr, kableExtra)
+
+# common theme to add to all ggplot plots
+mytheme <- theme(legend.position = "bottom") + theme_bw()
+# create a common format for all your tables
+create_table <- function(df){
+    df |>
+    kableExtra::kbl(digits = 3) |>
+    kableExtra::kable_styling(latex_options = c("HOLD_position", "scale_down"))
+}
 
 # Functions -----------------------------------------
